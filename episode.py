808a3720f5f76fef0e2dd1d473a40bc04fdc58ae
Robert Wolfe <rob@portwallistechnologies.com>
2023-09-11 17:26:21 -0400

    fussing with the final bits for production

--- episode.py
+++ episode.py
@@ -1,1 +1,18 @@
+import zipfile
+import datetime
+import os
 
+
+last_run: datetime = datetime.datetime.now() - datetime.timedelta(4)
+
+datapath: str =  f'{os.curdir}\\.data'
+
+submissions_file: str = f"{datapath}\\submissions.zip"
+
+submissions = zipfile.PyZipFile(submissions_file)
+
+submissions_extracted = [
+    submissions.extract(a, datapath)
+    for a in submissions.infolist()
+    if datetime.datetime(*a.date_time) > last_run
+]
