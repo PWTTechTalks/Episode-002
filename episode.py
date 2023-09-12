18c4c0caa9582526899619bdc81f21a504ab06cd
Robert Wolfe <rob@portwallistechnologies.com>
2023-09-12 08:11:59 -0400

    final version of episode code

--- episode.py
+++ episode.py
@@ -1,41 +1,24 @@
 import zipfile
 import datetime
 import os
-import time
 
-# normally this would be "yesterday" -- timedelta(1) but since this was recorded on a monday ... 
-last_run: datetime = datetime.datetime.now() - datetime.timedelta(2)
+# This should really be an environmnent variable or  parameter or some such but we will set to midnight yesterday
+
+last_run: datetime = datetime.datetime.now().replace(hour=0,minute=0,second=0,microsecond=0) - datetime.timedelta(1)
+
+# in the interests of not having to type magic strings several times
 
 datapath: str = "c:\\.data"
 
 submissions_file: str = f"{datapath}\\submissions.zip"
 
-submissions = zipfile.PyZipFile(submissions_file)
+submissions: zipfile.PyZipFile = zipfile.PyZipFile(submissions_file)
 
-# important to understand the order of execution
-# the extract happens BEFORE the time gets set even though from the order in the file it looks like 
-# we set the time before we extract the file
-# if you find this confusing you can do a standard for loop but comprehensions are often faster
-start = time.time()
-[
-    os.utime(b[0], (b[1], b[1]))
-    for b in [
-        (submissions.extract(a, datapath), datetime.datetime(*a.date_time).timestamp())
-        for a in submissions.infolist()
-        if datetime.datetime(*a.date_time) > last_run
-    ]
-]
-end = time.time()
-print (end-start)
+new_submissions: list = [b for b in submissions.infolist() if datetime.datetime(*b.date_time) > last_run ]
 
-start = time.time()
-
-for c in submissions.infolist() :
-    if datetime.datetime(*c.date_time) > last_run:
-        thetime = datetime.datetime(*c.date_time).timestamp()
-        os.utime(submissions.extract(c,datapath),(thetime,thetime))
-
-end = time.time()
-print (end-start)
+for a  in new_submissions:
+    submissions.extract(a.filename, datapath)
+    thetime: float = datetime.datetime(*a.date_time).timestamp()
+    os.utime(datapath + "\\" + a.filename, (thetime, thetime))
         
-    
+print (f'From {len(submissions.infolist())} compressed files we extracted {len(new_submissions)} modified since {last_run}')
